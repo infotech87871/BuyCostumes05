9e2cd35b35ea44fe2792300de4b3e4c15cf04f3a
infotech87871 <infotech87871@gmail.com>
2023-09-07 22:20:02 +0530

    Push 02

--- src/test/java/org/gitpush05/GitHubPush05.java
+++ src/test/java/org/gitpush05/GitHubPush05.java
@@ -11,5 +11,15 @@ public class GitHubPush05 {
 	private void grape3() {
 		System.out.println("grape");
 	}
+	private void cucumber4() {
+		System.out.println("orange");
+	}
+	private void pineApple5() {
+		System.out.println("apple");
+	}
+	private void banana6() {
+		System.out.println("grape");
+	}
+
 	
 }
