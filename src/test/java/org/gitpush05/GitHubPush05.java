1e1e45d0fffa29e528b3b10a2525abbdbdadf238
infotech87871 <infotech87871@gmail.com>
2023-09-07 22:11:26 +0530

    Push 01

---
+++ src/test/java/org/gitpush05/GitHubPush05.java
@@ -0,0 +1,15 @@
+package org.gitpush05;
+
+public class GitHubPush05 {
+
+	private void orange1() {
+		System.out.println("orange");
+	}
+	private void apple2() {
+		System.out.println("apple");
+	}
+	private void grape3() {
+		System.out.println("grape");
+	}
+	
+}
