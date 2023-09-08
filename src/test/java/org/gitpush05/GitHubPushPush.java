0a3aa2866ada96d3f56d6118eba371d2d2d03bd0
infotech87871 <infotech87871@gmail.com>
2023-09-08 10:52:17 +0530

    Push 04 Another Class

---
+++ src/test/java/org/gitpush05/GitHubPushPush.java
@@ -0,0 +1,20 @@
+package org.gitpush05;
+
+public class GitHubPushPush {
+
+	private void grape3() {
+		System.out.println("grape");
+	}
+	private void cucumber4() {
+		System.out.println("orange");
+	}
+	private void pineApple5() {
+		System.out.println("apple");
+	}
+	private void banana6() {
+		System.out.println("grape");
+	}
+	private void tomato4() {
+		System.out.println("tomato4");
+	}
+}
